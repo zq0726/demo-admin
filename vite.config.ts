4c79c8766183db1583282d6da82a3e0d8025b7ee
赵庆 <805322803@qq.com>
2024-12-25 14:53:02 +0800

    feat: 配置 unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
-  base: '/demo-admin/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('env', env)
+  console.log('command', command)
+
+  return {
+    plugins: [vue(), vueJsx(), vueDevTools(), UnoCSS()],
+    base: '/demo-admin/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })

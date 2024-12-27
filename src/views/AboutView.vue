10d71a11be64fa15b6a15ec4042bf76177c70dc3
赵庆 <805322803@qq.com>
2024-12-27 17:30:05 +0800

    fix: 配置 title

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,26 +1,10 @@
-<script setup lang="ts">
-import { userLoginApi } from '@/api/about'
-
-const getInfo = async () => {
-  try {
-    const res = await userLoginApi({
-      username: 'admin',
-      password: '123456789',
-    })
-
-    console.log('res', res)
-    console.log(res.token)
-  } catch (error) {
-    console.log('error33333333', error)
-  }
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="bg-red">
     <h1>This is an about page</h1>
 
-    <button @click="getInfo">调用接口</button>
+    <button>调用接口</button>
   </div>
 </template>
 

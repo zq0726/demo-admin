ccbdb11bea41125fdc6077a2674f896c9c0e2637
赵庆 <805322803@qq.com>
2024-12-27 17:27:37 +0800

    fix: 配置 title

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -6,6 +6,7 @@ export default {
     'stylelint-config-recommended-vue/scss',
   ],
   plugins: ['stylelint-order'],
+
   rules: {
     'no-descending-specificity': null,
     'function-url-quotes': 'always',
